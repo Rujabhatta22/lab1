f1651dba387d6837ecfb3e540743477de8941823
RUJA <rujabhatta22@gmail.com>
2021-02-25 09:35:33 +0545

    Initial commit

---
+++ ruja/rujaaaaaa.py
@@ -0,0 +1,7 @@
+# write a program that takes three number and print their sum. every number is given on a separate line.
+num1 = int(input("enter the first value: "))
+num2 = int(input("enter the second value: "))
+num3 = int(input("enter the third value: "))
+sum = num1 + num2 + num3
+print(f"The sum of given three numbers is {sum}")
+
